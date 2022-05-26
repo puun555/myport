162444b811411172d38781ddecdb6c71380f2392
puun555 <105053170+puun555@users.noreply.github.com>
2022-05-26 18:41:59 +0700

    lastupdate portfolio
    bye bitch

---
+++ script.js
@@ -0,0 +1,10 @@
+// let htmlq = document.getElementById("html-info")
+// let hovere = document.getElementById("htmllang")
+
+// hovere.addEventListener("mouseover", mouseOver);
+// hovere.addEventListener("mouseout", mouseOut);
+
+
+// function mouseOver() {
+//     htmlq.style.top = "25vh"
+// }
